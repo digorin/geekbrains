10e8ff7cf232048e7ab281b0dd878cb4b0d18e01
digorin <digorin@yandex.ru>
2020-05-06 16:36:32 +0300

    Finished 1-5 examples from lesson2

---
+++ Zadanie2-4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2_4
+{
+    class Program
+    {
+        //4. Реализовать метод проверки логина и пароля.На вход подается логин и пароль.На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains).
+        //Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает.
+        //С помощью цикла do while ограничить ввод пароля тремя попытками.
+        //Горин Дмитрий
+
+        static void Main(string[] args)
+        {
+            string[] result = new string[2];
+            int tryCount = 0;
+            do
+            {
+                result = Input();
+                if (Authorization(result[0], result[1]))
+                {
+                    Console.WriteLine("Доступ разрешен.");
+                    Console.ReadKey();
+                    return; //Тут вожет быть значение true, а метод использоваться по аналогии с Authorization для получения доступа к продолжению выполнения кода из вызывающего класса.
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный логин или пароль. Осталось {2-tryCount} попытки");
+                }
+                tryCount++;
+            }
+            while (tryCount < 3);
+            Console.WriteLine("Вы исчерпали попытки");
+            Console.ReadKey();
+            }
+
+        /// <summary>
+        /// Метод проверяет правильность логина и пароля
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        static bool Authorization(string login, string password)
+        {
+            string login_true = "root";
+            string password_true = "GeekBrains";
+
+            if(login_true == login && password_true == password)
+            {
+                return true;  
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Метод запрашивает число и не завершается пока пользователь его не введет
+        /// </summary>
+        /// <param name="question">Вопрос, который выводится пользователю перед вводом</param>
+        /// <returns></returns>
+        static string[] Input()
+        {
+            string[] result = new string[2];
+            Console.WriteLine("Введите логин");
+            result[0] = Console.ReadLine().ToString();
+            Console.WriteLine("Введите пароль");
+            result[1] = Console.ReadLine().ToString();
+            return result;
+        }
+    }
+}
---
+++ Zadanie2-5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2_5
+{
+    class Program
+    {
+        //5. а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или все в норме;
+        //б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+        //Горин Дмитрий
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите ваш рост в метрах");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Укажите ваш вес в килограммах");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            double index = weight / Math.Pow(height,2);
+
+            Console.WriteLine($"Ваш индекс массы тела: {index}.");
+            HowAreYou(index);
+            WhatToDo(index, height, weight);
+            Console.ReadLine();
+
+
+        }
+
+        static void HowAreYou(double index)
+        {
+            if (index >= 18.5 && index <= 25)
+            {
+                Console.WriteLine("Всё хорошо.");
+
+            }
+            else if (index > 25)
+            {
+                Console.WriteLine("Пора худеть!");
+            }
+            else
+            {
+                Console.WriteLine("Ты дрищ!");
+            }
+        }
+
+        static double WhatToDo(double index, double height, double weight)
+        {
+            double new_weight = 0;
+            if (index >= 18.5 && index <= 25)
+            {
+                return new_weight;
+            }
+            else if (index > 25)
+            {
+                new_weight = weight - (25 * Math.Pow(height, 2));
+                Console.WriteLine($"Вам надо скинуть минимум {new_weight} килограммов.");
+                return new_weight;
+            }
+            else
+            {
+                new_weight = (18.5 * Math.Pow(height, 2)) - weight;
+                Console.WriteLine($"Вам надо набрать минимум {new_weight} килограммов.");
+                return new_weight;
+            }
+        }
+    }
+}
