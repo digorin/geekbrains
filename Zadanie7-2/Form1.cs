1e875db6b48251034bc018345d6238030c0b03e7
digorin <digorin@yandex.ru>
2020-05-27 00:05:32 +0300

    Lesson 7 done

---
+++ Zadanie7-2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie7_2
+{
+    public partial class Form1 : Form
+    {
+
+        //Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток.Компьютер говорит, больше или меньше загаданное число введенного.
+        //a) Для ввода данных от человека используется элемент TextBox;
+        //Горин Дмитрий
+
+        Guess guess = new Guess();
+        int status = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CheckEnd(Guess guess)
+        {
+            if (guess.Finished == true)
+            {
+                label_tryCount.Text = "";
+                label_message.Text = "";
+                guess = new Guess();
+            }
+
+        }
+
+        private void button_check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                status = guess.Check(int.Parse(textBox1.Text));
+                label_tryCount.Text = guess.Count.ToString();
+                label_message.Text = guess.message;
+                if(status == 1)
+                {
+                    CheckEnd(guess);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных");
+            }
+        }
+    }
+}
---
+++ Zadanie7-2/Guess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie7_2
+{
+    class Guess
+    {
+
+        public int Number { get; private set; }
+        public string message = "";
+        int count = 0;
+        bool finished = false;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public bool Finished
+        {
+            get { return finished; }
+        }
+
+        public Guess()
+        {
+            Number = new Random().Next(1, 101);
+        }
+
+        public int Check(int userguess)
+        {
+            count++;
+            if(userguess == Number)
+            {
+                message = "Вы угадали. Игра окончена";
+                finished = true;
+                return 1;
+            }
+            else
+            {
+                if (userguess > Number)
+                {
+                    message = "Искомое число меньше Вашего";
+                }
+                else { message = "Искомое число больше Вашего"; }
+                return -1;
+            }
+        }
+    }
+}
